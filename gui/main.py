37581044933c535dd7085866e64185de204b989d
Michael-Yongshi <39827427+Michael-Yongshi@users.noreply.github.com>
2020-08-08 17:44:47 +0200

    navigation

--- gui/main.py
+++ gui/main.py
@@ -83,15 +83,58 @@ class WorldOverview(QMainWindow):
 
     def set_nested_widget(self):
 
-        # vertical layout for top and char part
+        # vertical layout for left and right part
         overviewbox = QGridLayout()
-        # overviewbox.addWidget(self.set_topbox(), 0, 0, 1, 3)
-        # overviewbox.addWidget(self.set_botbox(), 1, 0, 3, 3)
+        overviewbox.addWidget(self.set_navbox(), 0, 0, 1, 1)
+        overviewbox.addWidget(self.set_pagebox(), 0, 1, 1, 3)
 
         overviewboxframe = QBorderlessFrame()
         overviewboxframe.setLayout(overviewbox)
 
         return overviewboxframe
+
+    def set_navbox(self):
+
+        # vertical layout for left and right part
+        navbox = QVBoxLayout()
+
+        storybtn = QPushButton()
+        storybtn.setText("Story")
+        navbox.addWidget(storybtn)
+
+        timebtn = QPushButton()
+        timebtn.setText("Timelines")
+        navbox.addWidget(timebtn)
+
+        localebtn = QPushButton()
+        localebtn.setText("Locations")
+        navbox.addWidget(localebtn)
+
+        charbtn = QPushButton()
+        charbtn.setText("Characters")
+        navbox.addWidget(charbtn)
+
+        eventbtn = QPushButton()
+        eventbtn.setText("Events")
+        navbox.addWidget(eventbtn)
+
+        navboxframe = QBorderlessFrame()
+        navboxframe.setLayout(navbox)
+
+        return navboxframe
+
+
+    def set_pagebox(self):
+
+        # vertical layout for left and right part
+        pagebox = QVBoxLayout()
+
+        # pagebox.addWidget(eventbtn)
+
+        pageboxframe = QBorderlessFrame()
+        pageboxframe.setLayout(pagebox)
+
+        return pageboxframe
 
 def run():
     global app
